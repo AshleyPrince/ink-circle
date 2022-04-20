6ea6f648dfc1b6cb09e89ec566bd1ecc7eb45318
Ashley <ashleyeffenberger@gmail.com>
2022-04-20 17:30:44 +0200

    breakpoint bearbeitet

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,6 +2,6 @@ let allP = document.querySelectorAll('p');
 
 allP.forEach((element) => {
     let str = element.outerHTML
-    newStr = str.replace(/ß/g, '<span class="neu">ẞ</span>')
+    newStr = str.replace(/ß/g, '<span class="fix-s-z">ẞ</span>')
     element.outerHTML = newStr
 })
